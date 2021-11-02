5963b8e75a5a48541eac8b64c3a0fe6fe9d31ab0
Lucas Ferracin <80696823+lucastoupitzen@users.noreply.github.com>
2021-11-02 08:48:33 -0300

    Update cerebro.js

--- cerebro.js
+++ cerebro.js
@@ -7,7 +7,7 @@ function filtro() {
     num.innerHTML = '<p>Insira a parte numérica: </p><input type="number" name="quantia" id="quantia">'
     switch(tipo.value) {
         case "distancia":
-             res.innerHTML = '<p>Selecione a unidade de medida atual: </p><select id="distancia"><option value="inicio">Selecione</option><option value="centimetro">Centímetro</option><option value = "quilometro">Quilometro</option><option value="metro">Metro</option><option value="pé">Pé</option></select>'
+             res.innerHTML = '<p>Selecione a unidade de medida atual: </p><select id="distancia"><option value="inicio">Selecione</option><option value="centímetro">Centímetro</option><option value = "quilometro">Quilometro</option><option value="metro">Metro</option><option value="pé">Pé</option></select>'
              outp.innerHTML = '<p>Selecione a unidade de medida desejada: </p><select id="distanciadesejada"><option value="inicio">Selecione</option><option value="centimetro">Centímetro</option><option value = "quilometro">Quilometro</option><option value="metro">Metro</option><option value="pé">Pé</option></select>'
              bot.innerHTML = '<input type="button" id="disparo" value="Calcular" onclick="calculadora_distancia()"></input>'
              break
@@ -16,6 +16,11 @@ function filtro() {
              outp.innerHTML = '<p>Selecione a unidade de medida desejada: </p><select id="massadesejada"><option value="inicio">Selecione</option><option value="grama">Gramas</option><option value = "quilo">Quilograma</option><option value="libra">Libra</option><option value="onça">Onça</option></select>'
              bot.innerHTML = '<input type="button" id="disparo" value="Calcular" onclick="calculadora_massa()"></input>'
              break
+        case "volume":
+            res.innerHTML = '<p>Selecione a unidade de medida atual: </p><select id="volume"><option value="inicio">Selecione</option><option value="litros">Litro</option><option value = "m³">m³</option></select>'
+            outp.innerHTML = '<p>Selecione a unidade de medida desejada: </p><select id="volumedesejado"><option value="inicio">Selecione</option><option value="litros">Litro</option><option value = "m³">m³</option></select>'
+            bot.innerHTML = '<input type="button" id="disparo" value="Calcular" onclick="calculadora_volume()"></input>'
+            break
         default: res.innerHTML = 'Deu errado'
     }
 }
@@ -24,7 +29,7 @@ function calculadora_distancia() {
     var inputini = document.getElementById("distancia").value
     var inputfin = document.getElementById("distanciadesejada").value
     var dadosMedidas = {
-        medidas: ['centimetro','quilometro','metro','pé'],
+        medidas: ['centímetro','quilometro','metro','pé'],
         quantRel: [0.01,1000,1,0.3048]
     }
     var controleEntrada = 0;
@@ -71,4 +76,30 @@ function calculadora_massa() {
     var resultado = dadosMedidas.quantRel[controleEntrada] / dadosMedidas.quantRel[controleSaida]
     var output = document.getElementById("outpu")
     output.innerHTML = `${inputnum} ${inputini}s são ${(inputnum*resultado)} ${inputfin}s`
+}
+function calculadora_volume() {
+    var inputnum = document.getElementById("quantia").value
+    var inputini = document.getElementById("volume").value
+    var inputfin = document.getElementById("volumedesejado").value
+    var dadosMedidas = {
+        medidas: ['litros',"m³"],
+        quantRel: [1,1000]
+    }
+    var controleEntrada = 0;
+    for (i in dadosMedidas.medidas) {
+        if (dadosMedidas.medidas[i] == inputini) {
+        break
+        }
+        else controleEntrada ++
+    }
+    var controleSaida = 0;
+    for (i in dadosMedidas.medidas) {
+        if (dadosMedidas.medidas[i] == inputfin) {
+        break
+        }
+        else controleSaida ++
+    }
+    var resultado = dadosMedidas.quantRel[controleEntrada] / dadosMedidas.quantRel[controleSaida]
+    var output = document.getElementById("outpu")
+    output.innerHTML = `${inputnum} ${inputini} são ${(inputnum*resultado)} ${inputfin}`
 }
